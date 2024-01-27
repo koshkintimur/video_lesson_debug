550a752f05b9d5a9dbee15fb825446efa686648c
Timur Koshkin <koshkintimur@yandex.ru>
2024-01-27 20:11:42 +0500

    Debug dedovskim sposobom with func print()

--- 01_multifiles/utils.py
+++ 01_multifiles/utils.py
@@ -8,6 +8,9 @@ def get_second_number():
 
 def multiplier():
     first_number = get_first_number()
+    print(first_number)
     second_number = get_second_number()
+    print(second_number)
     product_of_numbers = first_number * second_number
+    print(product_of_numbers)
     return product_of_numbers
